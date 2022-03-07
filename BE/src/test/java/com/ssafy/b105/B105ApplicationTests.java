a8322d35068a210565774e76133acf9e2e8344d4
이동호 <edh1021@gmail.com>
2022-03-08 02:22:34 +0900

    Spring boot 2.6.4 project init

---
+++ BE/src/test/java/com/ssafy/b105/B105ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.b105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B105ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
