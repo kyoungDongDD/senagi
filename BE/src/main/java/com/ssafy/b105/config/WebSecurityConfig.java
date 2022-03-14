8c49bbb984bf76b7043dfd129675767432f59032
ddddd <rlarudehd32@naver.com>
2022-03-14 16:55:39 +0900

    - JWT 의존성 주입 추가 - JPA , H2 , jwt yml 추가

--- BE/src/main/java/com/ssafy/b105/config/WebSecurityConfig.java
+++ BE/src/main/java/com/ssafy/b105/config/WebSecurityConfig.java
@@ -1,15 +1,31 @@
 package com.ssafy.b105.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    super.configure(auth);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+  }
+
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -20,10 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .disable()
 
         .authorizeRequests()
-        .anyRequest().permitAll()
+        .antMatchers("api/").permitAll()
+        .anyRequest().authenticated()
         .and()
 
         .formLogin()
         .disable();
+  }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 }
