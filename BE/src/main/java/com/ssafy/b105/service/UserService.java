12e2eb8db9e3b7725995e3e476b3be2d7e68b050
ddddd <rlarudehd32@naver.com>
2022-03-18 01:32:14 +0900

    BE-feat-user-testSignUP S06P22B105-138 - 정규표현식 검증 추가 - 예외 추가 - 회원가입 테스트 코드 작성

--- BE/src/main/java/com/ssafy/b105/service/UserService.java
+++ BE/src/main/java/com/ssafy/b105/service/UserService.java
@@ -6,12 +6,14 @@ import com.ssafy.b105.entity.User;
 import com.ssafy.b105.entity.UserAuthority;
 import com.ssafy.b105.entity.UserType;
 import com.ssafy.b105.exception.DuplicateException;
+import com.ssafy.b105.exception.ExpressionValidateException;
 import com.ssafy.b105.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
@@ -26,14 +28,31 @@ public class UserService {
   //중복 체크
   @Transactional
   public UserDto signup(UserDto userDto) {
-    // 중복 검증증
 
-    //아이디 검증
+    //아이디 정규식 검증
+    if (userDto.getPrincipal().isBlank()) throw new NullPointerException("아이디를 입력하십시오.");
+    else if(!Pattern.matches("([\\w-.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$",userDto.getPrincipal().trim())){
+      throw new ExpressionValidateException("아이디 형식이 올바르지 않습니다.");
+    }
+
+    //비밀번호 정규식 검증
+    if (userDto.getCredential().isBlank()) throw new NullPointerException("비밀 번호를 입력하십시오.");
+    else if(!Pattern.matches("^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$",userDto.getCredential().trim())){
+      throw new ExpressionValidateException("비밀번호 형식이 올바르지 않습니다.");
+    }
+
+    //닉네임 정규식 검증
+    if (userDto.getName().isBlank()) throw new NullPointerException("닉네임을 입력하십시오.");
+    else if(!Pattern.matches("^[가-힣a-zA-Z]+$",userDto.getName().trim())){
+      throw new ExpressionValidateException("닉네임 형식이 올바르지 않습니다.");
+    }
+
+    //아이디 중복 검증
     if (userRepository.findOneWithAuthoritiesByPrincipal(userDto.getPrincipal()).orElse(null) != null) {
       throw new DuplicateException("이미 가입되어 있는 아이디 입니다.");
     }
 
-    //닉네임 검증
+    //닉네임 중복 검증
     if(userRepository.findOneByName(userDto.getName()).orElse(null) != null){
       throw new DuplicateException("이미 사용중인 이름 입니다.");
     }
