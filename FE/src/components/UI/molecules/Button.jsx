6c5ff1d94c5890dcbf19263694559937d7c7ac87
haein <haein.hannah.park@gmail.com>
2022-03-18 16:33:38 +0900

    feat: atom component-button added

--- FE/src/components/UI/molecules/Button.jsx
+++ FE/src/components/UI/molecules/Button.jsx
@@ -1,1 +1,22 @@
-// Icon + Text
+import { Button as Btn} from "@mui/material";
+
+function Button(props) {
+  return (
+    <div>
+      <Btn
+        type={props.type}
+        fullWidth={props.fullWidth}
+        variant={props.variant}
+        sx={{ mt: 3, mb: 2 }}
+        size="large"
+        style={{
+          backgroundColor: "#F4BA34",
+        }}
+      >
+        {props.text}
+      </Btn>
+    </div>
+  );
+}
+
+export default Button;
