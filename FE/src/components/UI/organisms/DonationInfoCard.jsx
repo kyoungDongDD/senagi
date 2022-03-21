2bfec329d5aaeaedf8d761f12e625e7344e6d699
beck <backcy1@daum.net>
2022-03-21 10:48:17 +0900

    feat : DonationInfoCard,NavBar 완성

--- FE/src/components/UI/organisms/DonationInfoCard.jsx
+++ FE/src/components/UI/organisms/DonationInfoCard.jsx
@@ -1,2 +1,49 @@
 // Text, ProgressBar
 // D-Day, Button
+import * as React from 'react';
+import styled from '@emotion/styled';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import ProgressBar from "./../molecules/ProgressBar";
+import Dday from '../molecules/D-Day';
+
+export default function ImgCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }} style={{ position: 'relative'}}>
+      <Dday />
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="200"
+        image={ require('../../../assets/test1.jpg')}
+        object-fit= {'cover'}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          경동이를 도와주세요
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          마석유기견보호소
+        </Typography>
+        <ProgressBar percent="0.5" />
+        <Money className='body1'>1,500,000원</Money>
+        <Progress className='body1'>50%</Progress>
+      </CardContent>
+    </Card>
+  );
+}
+
+const Money = styled.div`
+  margin-top: 15px;
+  display: inline-block
+  
+`
+const Progress = styled.div`
+  margin-top: 15px;
+  float: right;
+`
+
--- FE/src/components/UI/organisms/NavBar.jsx
+++ FE/src/components/UI/organisms/NavBar.jsx
@@ -1,28 +1,175 @@
-import { Outlet, Link } from "react-router-dom";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
-function NavBar() {
+import { Link } from "react-router-dom";
+
+
+const pages = ['캠페인', '사업소개', '마이페이지'];
+
+const NavBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: '#000000',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">캠페인</Link>
-          </li>
-          <li>
-            <Link to="/welcome">사업소개</Link>
-          </li>
-          <li>
-            <Link to="/mypage">마이페이지</Link>
-          </li>
-          <li>
-            <Link to="/login">로그인</Link>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-      <Outlet />
-    </div>
-  );
-}
+    // navbar배경색 변경 #F4BA3499
+    <AppBar position="static" style={{ background: 'inherit' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            <Link to="/">
+              <img
+                src={ require('../../../assets/logo.png')}
+                alt='logo'
+              />
+            </Link>
+          </Typography>
 
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            <Link to="/">
+              <img
+                src={ require('../../../assets/logo.png')}
+                alt='logo'
+              />
+            </Link>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center', marginLeft: '78px' }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                // 폰트 색,사이즈 변경
+                sx={{ my: 2, color: 'black', display: 'block', fontSize: '18px', paddingX: '40px' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="검색하기"
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                </Search>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default NavBar;
