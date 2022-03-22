e3fbf82198933c084ac0bab96b0e63bb74649646
haein <haein.hannah.park@gmail.com>
2022-03-22 22:41:58 +0900

    feat: 기부금 출금 신청 레이아웃

---
+++ FE/src/components/UI/molecules/BillTable.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('oo병원', 159, 6.0, 24, 4.0),
+  createData('xx병원', 237, 9.0, 37, 4.3),
+  createData('aa병원', 262, 16.0, 24, 6.0),
+];
+
+function BillTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>항목</TableCell>
+            <TableCell align="center">금액</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.calories}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default BillTable;
