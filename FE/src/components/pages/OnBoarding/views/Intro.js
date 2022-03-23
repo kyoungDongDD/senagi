16748b463b07d6c2ad7d5761f8ef97cee5f33abd
uiyn <uiyn@naver.com>
2022-03-23 18:34:26 +0900

    style: prettier 적용

--- FE/src/components/pages/OnBoarding/views/Intro.js
+++ FE/src/components/pages/OnBoarding/views/Intro.js
@@ -1,19 +1,20 @@
-import Button from "../component/Button";
-import Typography from "../component/Typography";
-import IntroLayout from "./IntroLayout";
+import Button from '../component/Button';
+import Typography from '../component/Typography';
+import IntroLayout from './IntroLayout';
 
 const backgroundImage =
-  "https://cdn.pixabay.com/photo/2017/12/27/14/02/friends-3042751_960_720.jpg";
+  'https://cdn.pixabay.com/photo/2017/12/27/14/02/friends-3042751_960_720.jpg';
 
 export default function Intro() {
   return (
     <IntroLayout
       sxBackground={{
         backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: "#7fc7d9",
-        backgroundPosition: "center",
+        backgroundColor: '#7fc7d9',
+        backgroundPosition: 'center',
       }}
     >
+      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
       <Typography color="inherit" align="center" variant="h3" marked="center">
         블록체인 기반
         <br />
@@ -21,12 +22,7 @@ export default function Intro() {
         <br />
         '세상에 나쁜 기부는 없다'
       </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
-      >
+      <Typography color="inherit" align="center" variant="h5" sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}>
         블록체인 기반 유기동물 기부 플랫폼
       </Typography>
       <Button
