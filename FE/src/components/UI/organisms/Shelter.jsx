ca530d0010be4f020d600e0971fd2d6f2a3e0592
uiyn <uiyn@naver.com>
2022-04-02 02:33:25 +0900

    feat : 보호소 세션 추가

--- FE/src/components/UI/organisms/Shelter.jsx
+++ FE/src/components/UI/organisms/Shelter.jsx
@@ -1,73 +1,68 @@
 import Carousel from 'nuka-carousel';
 import styled from '@emotion/styled';
-import { Grid, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import Text from '../atoms/Text';
 import MoreBtn from '../molecules/MoreButton';
 import Card from './DonationInfoCard';
 
 function Shelter() {
   const Container = styled.div`
-    background: linear-gradient(#fff 80%, #fffcf3 20%);
-  `;
-
-  const DotsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-  `;
-
-  const DotItem = styled.span`
-    cursor: pointer;
-    width: 12px;
-    height: 12px;
-    background: ${({ active }) => (active ? 'gray' : 'black')};
-    margin-bottom: 10px;
-    border-radius: 100px;
+    background: linear-gradient(#fff 70%, #fffcf3 30%);
   `;
 
   return (
     <>
       <Container>
-        <Box>
-          <Grid container direction="row" spacing={2}>
-            <Grid item container direction="column" xs spacing={2}>
-              <Grid item xs>
-                <Text className="header1" text="보호소 후원" />
-                <Text className="body2" text="관심있는 보호소에 후원하세요." />
-              </Grid>
-              <Grid item xs>
-                <MoreBtn />
-              </Grid>
-            </Grid>
-            <Grid item xs>
-              <div style={{ width: '40%', margin: 'auto' }}>
-                <Carousel
-                  autoplay={true}
-                  wrapAround={true}
-                  slideListMargin={0}
-                  slidesToScroll={1}
-                  slidesToShow={1}
-                  renderCenterLeftControls={null}
-                  renderCenterRightControls={null}
-                  renderBottomCenterControls={({ slideCount, currentSlide }) => (
-                    <DotsContainer>
-                      {Array.from(Array(slideCount).keys()).map((index) => {
-                        return (
-                          <DotItem
-                            active={currentSlide === index}
-                            key={`${index}_carousel_${Math.random()}`}
-                          />
-                        );
-                      })}
-                    </DotsContainer>
-                  )}
-                >
-                  <Card />
-                  <Card />
-                  <Card />
-                </Carousel>
-              </div>
-            </Grid>
-          </Grid>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', md: 'row' },
+            justifyContent: 'right',
+            mt: 30,
+            ml: 20,
+            mr: 20,
+            mb: 20,
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: { xs: 'center', md: 'flex-start' },
+              width: 900,
+            }}
+          >
+            <Box>
+              <Text className="header1" text="보호소 후원" />
+            </Box>
+            <Box>
+              <Text className="body2" text="관심있는 보호소에 후원하세요." />
+            </Box>
+            <Box>
+              <MoreBtn />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              alignItems: { xs: 'center', md: 'flex-start' },
+              width: 500,
+            }}
+          >
+            <Carousel
+              autoplay={true}
+              wrapAround={true}
+              slideListMargin={0}
+              slidesToScroll={1}
+              slidesToShow={1}
+              renderCenterLeftControls={null}
+              renderCenterRightControls={null}
+              renderBottomCenterControls={null}
+            >
+              <Card />
+              <Card />
+              <Card />
+            </Carousel>
+          </Box>
         </Box>
       </Container>
     </>
