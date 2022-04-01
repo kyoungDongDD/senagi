39f31631d43dd9507f9aee976db70809f2f816f3
haein <haein.hannah.park@gmail.com>
2022-04-01 14:12:20 +0900

    refactor: BASE_URL 및 회원가입 api 수정, 불필요한 코드 제거

--- FE/src/api/accountsAPI.js
+++ FE/src/api/accountsAPI.js
@@ -3,27 +3,27 @@ import api from './index';
 class AccountsAPI {
   // 회원가입
   signUp(accountData) {
-    return api.post(`/api/auth/signup/supporter`, JSON.stringify(accountData));
+    return api.post(`/auth/signup/supporter`, JSON.stringify(accountData));
   }
 
   // 이메일 중복확인
   checkEmail(userId) {
-    return api.get(`/api/auth/duplicate/principal?principal=${userId}`);
+    return api.get(`/auth/duplicate/principal?principal=${userId}`);
   }
 
   // 닉네임 중복확인
   checkName(name) {
-    return api.get(`/api/auth/duplicate/name?name=${name}`);
+    return api.get(`/auth/duplicate/name?name=${name}`);
   }
 
   // 후원자 로그인
   supporterLogin(accountData) {
-    return api.post(`/api/auth/login/supporter`, JSON.stringify(accountData));
+    return api.post(`/auth/login/supporter`, JSON.stringify(accountData));
   }
 
   // 보호소 로그인
   shelterLogin(accountData) {
-    return api.post(`/api/auth/login/shelter`, JSON.stringify(accountData));
+    return api.post(`/auth/login/shelter`, JSON.stringify(accountData));
   }
 }
 
