3e768651fd2c7ba331b45b2cdbd81bdd84c47bd0
박해인 <haein.hannah.park@gmail.com>
2022-04-06 20:47:43 +0900

    fix: console.log 주석 처리

--- FE/src/components/UI/organisms/GoogleLoginRedirect.jsx
+++ FE/src/components/UI/organisms/GoogleLoginRedirect.jsx
@@ -4,26 +4,24 @@ import { useDispatch } from 'react-redux';
 import { login, socialAuthSuccess } from '../../../store/user';
 import jwt from 'jwt-decode';
 
-// http://localhost:3000/sociallogin?jwtToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwiaXNzIjoiTm9CYWREb25hdGlvbiIsIm5pY2tuYW1lIjoiaGFlaW4uaGFubmFoLnBhcmtAZ21haWwuY29tIiwiaWQiOjIyLCJleHAiOjE2NDkzMTU3NzUsImlhdCI6MTY0OTIyOTM3NX0.OUIuh8rwkrCtKSRj_cyDd5zO2aYRD3_C67WgozyfBKE
-
 function GoogleLoginRedirect() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     let temp = new URL(window.location.href).searchParams.get('jwtToken');
-    console.log(temp);
+    // console.log(temp);
 
     const token = `{"jwtToken":"${temp}"}`;
     // const json = `{"jwtToken":"${temp}"}`;
     // const token = JSON.parse(json);
-    console.log('type of token', typeof token);
-    console.log('token', token);
+    // console.log('type of token', typeof token);
+    // console.log('token', token);
 
     if (token) {
       dispatch(login(token));
       const userInfo = jwt(token);
-      console.log('decoded userInfo', typeof userInfo);
+      // console.log('decoded userInfo', typeof userInfo);
       dispatch(socialAuthSuccess(userInfo));
       alert('로그인에 성공했습니다.');
       navigate('/home');
