98957f3f653309b3cd81a15aa16c69828b359670
bcy <backcy855@google.com>
2022-04-07 05:40:17 +0900

    사용내역API 연결, 이미지 연결

--- FE/src/components/UI/organisms/UsageHistory.jsx
+++ FE/src/components/UI/organisms/UsageHistory.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import ReceiptImage from './ReceiptImage';
 import CampaignCard from './CampaignCard';
+import PaymentAPI from '../../../api/paymentAPI';
 
 import styled from '@emotion/styled';
 import { Grid } from '@mui/material';
 
 function UsageHistory(props) {
-  const { targetDonation, lastModifiedDate, endDate, shelterName, balance, dday } = props;
+  const { targetDonation, lastModifiedDate, endDate, shelterName, balance, pageId, dday } = props;
+
+  const [Posts, setPosts] = useState('');
+
+  useEffect(() => {
+    PaymentAPI.getWithdraw(pageId).then((response) => {
+      const campaign = response.data;
+      setPosts(campaign);
+    });
+  }, []);
+
+  const receipImages = Posts.receiptImageUrl;
+
+  // amountAll 값이 없을 시 그대로 출력해서 오류 방지
+  const amount = Posts.amountAll ? Posts.amountAll.toLocaleString() : Posts.amountAll;
 
   return (
     <div>
       <LeyoutContainer>
         <Grid container justifyContent={'space-evenly'} marginTop={'30px'}>
           <Grid>
-            <ReceiptImage />
-            <p>사용금액 456,789원</p>
+            <ReceiptImage receipImages={receipImages} />
+            {amount ? (
+              <Usage>사용금액 : {amount}원</Usage>
+            ) : (
+              <NoUsage>사용 내역이 없습니다.</NoUsage>
+            )}
           </Grid>
           <Grid>
             <CardPosition>
@@ -46,4 +65,16 @@ const CardPosition = styled.div`
 
 const LeyoutContainer = styled.div`
   height: 700px;
+`;
+
+const Usage = styled.p`
+  text-align: center;
+  font-family: 'GM';
+  font-size: 32px;
+`;
+
+const NoUsage = styled.p`
+  text-align: center;
+  font-family: 'GM';
+  font-size: 32px;
 `;
