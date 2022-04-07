8fb1658fa1a6e6be9a8680c46e10806d8e4b8d94
uiyn <uiyn@naver.com>
2022-04-07 22:15:46 +0900

    design : footer 수정

--- FE/src/components/UI/organisms/Footer.jsx
+++ FE/src/components/UI/organisms/Footer.jsx
@@ -2,28 +2,33 @@ import styled from '@emotion/styled';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import { grey } from '@mui/material/colors';
 const Footer = () => {
   return (
     <div>
       <IconDiv>
         <div>
           <FacebookIcon
+            sx={{ color: grey[600] }}
             fontSize="large"
             onClick={() => window.open('https://www.facebook.com/')}
             cursor="pointer"
           />
           <InstagramIcon
+            sx={{ color: grey[600] }}
             fontSize="large"
             onClick={() => window.open('https://www.instagram.com/')}
             cursor="pointer"
           />
           <GitHubIcon
+            sx={{ color: grey[600] }}
             fontSize="large"
             onClick={() => window.open('https://github.com/')}
             cursor="pointer"
           />
         </div>
+        <P>김경동 김민현 이동호</P>
+        <P>박해인 백철연 이아현</P>
         <P>COPYRIGHTⓒ 2022 SENAGI ALL RIGHTS RESERVED.</P>
       </IconDiv>
     </div>
